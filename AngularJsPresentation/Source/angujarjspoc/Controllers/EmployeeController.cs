3d99f8a3b0c712cb5d04f5783bfa9588676dbbda
Paulo Ito <itopaulojr@gmail.com>
2015-05-11 23:21:54 -0300

    First files upload

---
+++ AngularJsPresentation/Source/angujarjspoc/Controllers/EmployeeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace angujarjspoc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        private List<Employee> Employees
+        {
+            get
+            {
+                if (Session["Employees"] == null)
+                {
+                    List<Employee> employees = new List<Employee>();
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Employee employee = new Employee();
+                        employee.Id = Guid.NewGuid().ToString();
+                        employee.Address = i.ToString();
+                        employee.AddressNumber = i.ToString();
+                        employee.Apartment = i.ToString();
+                        employee.BlockName = i.ToString();
+                        employee.Entrace = i.ToString();
+                        employee.Floor = i.ToString();
+                        employee.Name = i.ToString();
+                        employees.Add(employee);
+                    }
+                    Session["Employees"] = employees;
+                }
+
+                return (List<Employee>)Session["Employees"];
+            }
+        }
+
+        //
+        // GET: /Employee/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Employess()
+        {
+
+            return Json(Employees, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Employess(Employee newEmployee)
+        {
+            Employee currentEmployee = null;
+            if (string.IsNullOrEmpty(newEmployee.Id))
+            {
+                newEmployee.Id = Guid.NewGuid().ToString();
+                currentEmployee = newEmployee;
+                Employees.Add(currentEmployee);
+            }
+            else
+            {
+                currentEmployee = Employees.First(x => x.Id == newEmployee.Id);
+            }
+
+            currentEmployee.Name = newEmployee.Name;
+            currentEmployee.Address = newEmployee.Address;
+            currentEmployee.AddressNumber = newEmployee.AddressNumber;
+            currentEmployee.Apartment = newEmployee.Apartment;
+            currentEmployee.BlockName = newEmployee.BlockName;
+            currentEmployee.Entrace = newEmployee.Entrace;
+            currentEmployee.Floor = newEmployee.Floor;
+
+            return Json(currentEmployee, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string AddressNumber { get; set; }
+        public string BlockName { get; set; }
+        public string Entrace { get; set; }
+        public string Floor { get; set; }
+        public string Apartment { get; set; }
+        public string Type { get; set; }
+
+        public string Id { get; set; }
+    }
+}
---
+++ AngularJsPresentation/Source/angujarjspoc/Scripts/app.js
@@ -0,0 +1,114 @@
+﻿var myApp = angular.module('myApp', ['ngRoute', 'ngResource']);
+
+myApp.config(function ($routeProvider) {
+    $routeProvider
+
+    .when('/', {
+        templateUrl: '/Home/Home',
+        controller: 'HomeController'
+    })
+    .when('/Home/About', {
+        templateUrl: '/Home/About',
+        controller: 'AboutController'
+    })
+    .when('/Home/Contact', {
+        templateUrl: '/Home/Contact',
+        controller: 'ContactController'
+    })
+    .when('/Employee', {
+        templateUrl: '/Employee/Index',
+        controller: 'EmployeeController'
+    })
+    .when('/Account/Login', {
+        templateUrl: '/Account/Login',
+        controller: 'AccountLoginController'
+    })
+    .when('/Account/Register', {
+        templateUrl: '/Account/Register',
+        controller: 'AccountRegisterController'
+    })
+    .when('/Account/Manage', {
+        templateUrl: '/Account/Manage',
+        controller: 'AccountManageController'
+    });
+});
+
+myApp.controller('HomeController', ['$scope', '$log', function ($scope, $log) {
+    $scope.Name = "Home Uoww";
+
+}]);
+
+
+myApp.factory('employeeFactory', function ($resource) {
+    return $resource("/Employee/Employess", {}, {
+
+    });
+});
+
+myApp.controller('EmployeeController', ['$scope', '$window','$filter', 'employeeFactory', function ($scope, $window, $filter,employeeFactory) {
+    $scope.Employee = {};
+    $scope.Employee.Name = "Paulo";
+    $scope.Employee.Address = "Av. Três de Março";
+    $scope.Employee.AddressNumber = 1435;
+    $scope.Employee.BlockName = "Bl. 14";
+    $scope.Employee.Entrace = "abc";
+    $scope.Employee.Floor = 1;
+    $scope.Employee.Apartment = 103;
+    $scope.Employee.InternalEmail = null;
+    $scope.Employee.VendorCompanyName = null;
+    $scope.Employee.Type = 'Outsourcing';
+
+    $scope.Employees = [];
+
+    $scope.EmployeeTypes = [{ Id: 'Employee', Name: 'Employee' }, { Id: 'Outsourcing', Name: 'Outsourcing' }];
+
+    $scope.add = function () {
+
+        var newEmployee = true;
+        if($scope.Employee.Id)
+            newEmployee = false;
+
+        var employee = employeeFactory.save($scope.Employee, {}, function (data) {
+            if(newEmployee){
+                $scope.Employees.push(data);
+            }else{
+                var employee = $filter('filter')($scope.Employees, { Id: $scope.Employee.Id })[0];
+            }                
+            $scope.Employee = {};
+        });
+
+    }
+
+    $scope.delete = function (index) {
+        //$window.alert('teste');
+        $scope.Employees.splice(index, 1);
+    }
+
+    $scope.clear = function () {
+        //$scope.Employees = employeeFactory.query();
+        $scope.Employee = {};        
+    }
+
+    $scope.edit = function (index) {
+        var employee = $scope.Employees[index];
+        employee.Index = index;
+        $scope.Employee = employee;
+    }
+
+}]);
+
+myApp.controller('AboutController', ['$scope', '$log', function ($scope, $log) {
+    $scope.Name = "About Uoww";
+}]);
+
+myApp.controller('ContactController', ['$scope', '$log', function ($scope, $log) {
+    $scope.Name = "Contact Uoww";
+}]);
+
+myApp.controller('AccountLoginController', ['$scope', '$log', function ($scope, $log) {
+    $scope.Name = "Account Login Uoww";
+}]);
+
+myApp.controller('AccountRegisterController', ['$scope', '$log', function ($scope, $log) {
+    $scope.Name = "Account Login Uoww";
+}]);
